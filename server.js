ede81b9a9b5c93d81c674d3c77532b0da0a63cb8
Emerson Chiabai Furlan <emersonfurlanpc@gmail.com>
2020-03-07 17:30:52 -0300

    Reestruturação de arquivos

--- server.js
+++ server.js
@@ -9,17 +9,8 @@ const app = express();
 mongoose.connect('mongodb://localhost:27017/nodeapi',{ useNewUrlParser: true, useUnifiedTopology: true });
 requireDir('./src/models');
 
-const Product = mongoose.model('Product');
-
-
-// Primeira Rota
-app.get('/', (req, res) => {
-    Product.create({
-        title: 'React Native',
-        description: 'Build native apps with',
-        url: 'http://github.com/facebook/react-native'
-    });
-    return res.send("Hello Rocketseat");
-});
+// Rotas
+//Use - Aceita todos os tipos de requisição 
+app.use('/api', require("./src/routes"));
 
 app.listen(3001);
